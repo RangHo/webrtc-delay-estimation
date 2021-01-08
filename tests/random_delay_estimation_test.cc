990e4d10feac513e2f6a95fa3771269a5bff655e
RangHo Lee <hello@rangho.me>
2021-01-08 17:00:03 +0900

    Use regular testcases instead of BDD-style ones

--- tests/random_delay_estimation_test.cc
+++ tests/random_delay_estimation_test.cc
@@ -10,86 +10,81 @@
 
 #include "test_tools.h"
 
-SCENARIO("random sample should produce correct delay", "[delay_estimation]") {
-  GIVEN("there is a random sample") {
-    constexpr size_t kNumRenderChannels = 1;
-    constexpr size_t kNumCaptureChannels = 1;
-    constexpr int kSampleRateHz = 48000;
-    constexpr size_t kNumBands = kSampleRateHz / 16000;
+TEST_CASE("random sample should produce correct delay") {
+  constexpr size_t kNumRenderChannels = 1;
+  constexpr size_t kNumCaptureChannels = 1;
+  constexpr int kSampleRateHz = 48000;
+  constexpr size_t kNumBands = kSampleRateHz / 16000;
 
-    constexpr size_t kDownSamplingFactors[] = {2, 4, 8};
-    constexpr size_t kDelaySamples[] = {30, 64, 150, 200, 800, 4000};
+  constexpr size_t kDownSamplingFactors[] = {2, 4, 8};
+  constexpr size_t kDelaySamples[] = {30, 64, 150, 200, 800, 4000};
 
-    INFO("Creating render and capture buffers.");
-    // Render buffer
-    std::vector<std::vector<std::vector<float>>> render(
-        kNumBands,
-        std::vector<std::vector<float>>(
-            kNumRenderChannels, std::vector<float>(webrtc::kBlockSize)));
-    // Capture buffer
-    std::vector<std::vector<float>> capture(
-        kNumCaptureChannels, std::vector<float>(webrtc::kBlockSize));
+  INFO("Creating render and capture buffers.");
+  // Render buffer
+  std::vector<std::vector<std::vector<float>>> render(
+      kNumBands,
+      std::vector<std::vector<float>>(kNumRenderChannels,
+                                      std::vector<float>(webrtc::kBlockSize)));
+  // Capture buffer
+  std::vector<std::vector<float>> capture(
+      kNumCaptureChannels, std::vector<float>(webrtc::kBlockSize));
 
-    // Create no-op data dumper
-    webrtc::ApmDataDumper data_dumper(0);
-    for (auto down_sampling_factor : kDownSamplingFactors) {
-      // Create echo detector configuration
-      webrtc::EchoCanceller3Config config;
-      config.delay.down_sampling_factor = down_sampling_factor;
-      config.delay.num_filters = 10;
+  // Create no-op data dumper
+  webrtc::ApmDataDumper data_dumper(0);
+  for (auto down_sampling_factor : kDownSamplingFactors) {
+    // Create echo detector configuration
+    webrtc::EchoCanceller3Config config;
+    config.delay.down_sampling_factor = down_sampling_factor;
+    config.delay.num_filters = 10;
 
-      INFO("Created a configuration object: {"
-           << "down_sampling_factor: " << config.delay.down_sampling_factor
-           << ", num_filters: " << config.delay.num_filters << "}");
+    INFO("Created a configuration object: {"
+         << "down_sampling_factor: " << config.delay.down_sampling_factor
+         << ", num_filters: " << config.delay.num_filters << "}");
 
-      WHEN("the down sampling factor is " +
-           std::to_string(down_sampling_factor)) {
-        for (auto delay_samples : kDelaySamples) {
-          WHEN("the delay sample count is " + std::to_string(delay_samples)) {
-            std::unique_ptr<webrtc::RenderDelayBuffer> render_delay_buffer(
-                webrtc::RenderDelayBuffer::Create(config, kSampleRateHz,
-                                                  kNumRenderChannels));
-            DelayBuffer signal_delay_buffer(delay_samples);
-            webrtc::EchoPathDelayEstimator estimator(&data_dumper, config,
-                                                     kNumCaptureChannels);
+    for (auto delay_samples : kDelaySamples) {
+      SECTION("the down sampling factor is " +
+              std::to_string(down_sampling_factor) +
+              " and the delay sample count is " + std::to_string(delay_samples)) {
 
-            INFO("Start processing the blocks...");
-            CAPTURE(down_sampling_factor, delay_samples,
-                    500 + delay_samples / webrtc::kBlockSize);
+        std::unique_ptr<webrtc::RenderDelayBuffer> render_delay_buffer(
+            webrtc::RenderDelayBuffer::Create(config, kSampleRateHz,
+                                              kNumRenderChannels));
+        DelayBuffer signal_delay_buffer(delay_samples);
+        webrtc::EchoPathDelayEstimator estimator(&data_dumper, config,
+                                                 kNumCaptureChannels);
 
-            absl::optional<webrtc::DelayEstimate> estimated_delay_samples;
-            for (size_t i = 0; i < 500 + delay_samples / webrtc::kBlockSize;
-                 i++) {
-              RandomizeSampleVector(render[0][0]);
-              signal_delay_buffer.Delay(render[0][0], capture[0]);
-              render_delay_buffer->Insert(render);
+        INFO("Start processing the blocks...");
+        CAPTURE(down_sampling_factor, delay_samples,
+                500 + delay_samples / webrtc::kBlockSize);
 
-              if (i == 0) {
-                render_delay_buffer->Reset();
-              }
+        absl::optional<webrtc::DelayEstimate> estimated_delay_samples;
+        for (size_t i = 0; i < 500 + delay_samples / webrtc::kBlockSize; i++) {
+          RandomizeSampleVector(render[0][0]);
+          signal_delay_buffer.Delay(render[0][0], capture[0]);
+          render_delay_buffer->Insert(render);
 
-              render_delay_buffer->PrepareCaptureProcessing();
-
-              auto estimate = estimator.EstimateDelay(
-                  render_delay_buffer->GetDownsampledRenderBuffer(), capture);
-
-              if (estimate)
-                estimated_delay_samples = estimate;
-            }
-
-            THEN("there is an accurate estimation of delay") {
-              REQUIRE(estimated_delay_samples.has_value());
-
-              // Allow estimated delay to be off by one sample in the
-              // down-sampled domain.
-              size_t delay_ds = delay_samples / down_sampling_factor;
-              size_t estimated_delay_ds =
-                  estimated_delay_samples->delay / down_sampling_factor;
-              REQUIRE(estimated_delay_ds >= delay_ds - 1);
-              REQUIRE(estimated_delay_ds <= delay_ds + 1);
-            }
+          if (i == 0) {
+            render_delay_buffer->Reset();
           }
+
+          render_delay_buffer->PrepareCaptureProcessing();
+
+          auto estimate = estimator.EstimateDelay(
+              render_delay_buffer->GetDownsampledRenderBuffer(), capture);
+
+          if (estimate)
+            estimated_delay_samples = estimate;
         }
+
+        REQUIRE(estimated_delay_samples.has_value());
+
+        // Allow estimated delay to be off by one sample in the
+        // down-sampled domain.
+        size_t delay_ds = delay_samples / down_sampling_factor;
+        size_t estimated_delay_ds =
+            estimated_delay_samples->delay / down_sampling_factor;
+        REQUIRE(estimated_delay_ds >= delay_ds - 1);
+        REQUIRE(estimated_delay_ds <= delay_ds + 1);
       }
     }
   }
